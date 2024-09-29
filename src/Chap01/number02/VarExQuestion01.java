155b605d0c3fa00359152511e2429d73ea993187
OurOh <ahn980321@gmail.com>
2024-09-29 22:12:22 +0900

    0929 test

---
+++ src/Chap01/number02/VarExQuestion01.java
@@ -0,0 +1,16 @@
+package Chap01.number02;
+
+public class VarExQuestion01 {
+    /*문제1
+    다음 코드에 반복해서 나오는 숫자 `4` , `3` 을 다른 숫자로 한번에 변경할 수 있도록 다음을 변수 `num1` , `num2` 를 사용하
+    도록 변경해보세요.*/
+
+    public class VarEx1Question {
+        public static void main(String[] args) {
+            System.out.println(4 + 3);
+            System.out.println(4 - 3);
+            System.out.println(4 * 3);
+        }
+    }
+
+}
